28a1975b24bdff45a2d2f6b00530d1706d01c7c7
Erin Anderson <EAnderso@idem.IN.gov>
2022-08-10 15:47:24 -0400

    Add project files.

---
+++ Models/ErrorViewModel.fs
@@ -0,0 +1,8 @@
+namespace GoofyTestApp
+
+open System
+
+type ErrorViewModel private () =
+    member val RequestId : string = null with get, set
+
+    member val ShowRequestId : bool = true with get, set
